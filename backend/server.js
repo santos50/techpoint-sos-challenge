b3f426f30aee9cd5acf33f3ba70e4f6bf181d6ec
Dianne Santos <santos50@purdue.edu>
2020-06-30 21:48:27 -0400

    Update server.js
    fix deployment problem??

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json());
