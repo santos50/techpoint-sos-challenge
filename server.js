3174123abcb150f7bfc10bcf483be8bd93afcf80
Dianne Santos <santos50@purdue.edu>
2020-07-02 17:32:48 -0400

    getting server and client to operate concurrently

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json());
