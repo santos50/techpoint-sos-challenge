2fbb92fe946d2f33975dc1aace74aa2c4fdf92fd
Dianne Santos <santos50@purdue.edu>
2020-07-06 01:30:34 -0400

    issue

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const passport = require("passport");
+const path = require('path');
 // const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 5000;
