c411ed5c1a2ea0032ae5a12acff8ebd5e3f62d52
Dianne Santos <santos50@purdue.edu>
2020-07-13 18:19:37 -0400

    login here

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -160,7 +160,7 @@ class Login extends Component {
 
                 <button type="submit" className="btn btn-primary btn-block">Submit</button>
                 <p className="forgot-password text-right">
-                    Don't have an account? <a href="/sign-up" component={SignUp}>Sign up</a>
+                    HERE!! Don't have an account? <a href="/sign-up" component={SignUp}>Sign up</a>
                 </p>
             </form>
           </div>
